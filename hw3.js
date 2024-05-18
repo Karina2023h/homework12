2dab09a0892a5e09400713f9095900a3b26d6e3c
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2024-05-18 15:02:52 +0200

    xcfghn

--- hw3.js
+++ hw3.js
@@ -1,37 +1,33 @@
- const taskList = document.getElementById("taskList");
+const taskBlock = document.getElementById("taskList");
 
-    taskList.addEventListener("click", function(event) {
-        if (event.target.classList.contains("deleteButton")) {
-            const taskItem = event.target.parentElement;
-            taskItem.remove();
-        }
-    });
+taskBlock.addEventListener("click", function (e) {
+  if (e.target.classList.contains("deleteButton")) {
+    const itemBlock = e.target.parentElement;
+    itemBlock.remove();
+  }
+});
+const taskButton = document.getElementById("taskButton");
+const newTask = document.getElementById("newTask");
 
-    const addTaskButton = document.getElementById("addTaskButton");
-    const newTaskInput = document.getElementById("newTaskInput");
+taskButton.addEventListener("click", function () {
+  const taskText = newTask.value.trim();
+  if (taskText !== "") {
+    const itemBlock = document.createElement("li");
+    itemBlock.classList.add("task-item");
+    itemBlock.textContent = taskText;
 
-     addTaskButton.addEventListener("click", function() {
-        const taskText = newTaskInput.value.trim();
-        if (taskText !== "") {
-            const taskItem = document.createElement("li");
-            taskItem.classList.add("task-item");
-            taskItem.textContent = taskText;
-
-            const deleteButton = document.createElement("button");
-            deleteButton.classList.add("deleteButton");
-            deleteButton.textContent = "Видалити";
-
-            taskItem.appendChild(deleteButton);
-            taskList.appendChild(taskItem);
-
-            newTaskInput.value = "";
-        } else {
-            alert("Будь ласка, введіть текст завдання!");
-        }
-    });
-
-    newTaskInput.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            addTaskButton.click();
-        }
-    });
+    const deletButton = document.createElement("button");
+    deletButton.classList.add("deleteButton");
+    deletButton.textContent = "Видалити";
+    itemBlock.appendChild(deletButton);
+    taskBlock.appendChild(itemBlock);
+    newTask.value = "";
+  } else {
+    alert("Будь ласка, введіть текст завдання!");
+  }
+});
+newTask.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    taskButton.click();
+  }
+});
